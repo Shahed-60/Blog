d4c645a14106e74e4d699b77422666826979176f
Shahed Amer <chanyeol6521354@gmail.com>
2024-03-27 13:41:52 +0100

    The second way to how add a blade layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="css/app.css">
+<title>My blog</title>
+
+<body>
+    {{-- The first way how to add blade layout --}}
+    @yield('content')
+    {{-- The second way how to add blade layout --}}
+    {{-- {{ $slot }} --}}
+</body>
+
+</html>
